ca15f7331bf1f3f7bbdd9f660400cf96074b405d
EC2 Default User <ec2-user@ip-172-31-86-58.ec2.internal>
2019-05-27 06:58:40 +0000

    2705

--- Bacheca/src/app/app.module.ts
+++ Bacheca/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule , FormsModule} from '@angular/forms';
 import { ElencoComponent } from './elenco/elenco.component'; //aggiungo gli import per le from
@@ -11,7 +11,7 @@ import { ElencoComponent } from './elenco/elenco.component'; //aggiungo gli impo
     ElencoComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule, FormsModule //aggiungo
+    BrowserModule, ReactiveFormsModule, FormsModule, NgbModule //aggiungo
   ],
   providers: [],
   bootstrap: [AppComponent]
